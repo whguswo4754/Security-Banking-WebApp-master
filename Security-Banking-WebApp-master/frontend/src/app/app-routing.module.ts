58d0baa7d7b339b38f37066b3f13af7f0eae195b
whguswo4754 <81290172+whguswo4754@users.noreply.github.com>
2021-04-21 23:38:45 +0900

    Add files via upload

---
+++ Security-Banking-WebApp-master/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {ManageComponent} from "./components/manage/manage.component";
+
+
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'manage',component:ManageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
